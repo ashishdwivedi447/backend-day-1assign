6d15575e44cb70c96fd7f872ca8a08b355e1b1e6
Ashish Dwivedi <ad135787@gmail.com>
2022-03-03 21:55:56 +0530

    assignment completed

---
+++ main.js
@@ -0,0 +1,15 @@
+const addition=require("./add.js");
+
+console.log(addition(4,5));
+
+const division=require("./divide");
+
+console.log(division(8,4));
+
+const multiplication=require("./multiply.js");
+
+console.log(multiplication(3,2));
+
+const substraction=require("./substract.js");
+
+console.log(substraction(10,5));
